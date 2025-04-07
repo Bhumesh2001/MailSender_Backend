e8c17f0d203e5f2ba6fc734aaaaa4a18ebe898de
bhumesh2001 <bhumesh21@navgurukul.org>
2025-04-07 09:32:39 +0530

    initial commit

---
+++ server.js
@@ -0,0 +1,21 @@
+require('dotenv').config();
+const express = require("express");
+const http = require("http");
+const cors = require("cors");
+const { Server } = require("socket.io");
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server, { cors: { origin: "*" } });
+
+app.use(express.json());
+app.use(cors());
+
+app.use("/api/emails", require("./routes/emailRoutes")(io));
+
+io.on("connection", (socket) => {
+    console.log("🔌 Client connected!");
+    socket.on("disconnect", () => console.log("❌ Client disconnected"));
+});
+
+server.listen(5000, () => console.log(`🚀 Server running on port 5000`));
