a019435825d3f3747d4bd14644ffec0f1cdd1fd5
bhumesh2001 <bhumesh21@navgurukul.org>
2025-04-07 09:43:24 +0530

    new code

--- server.js
+++ server.js
@@ -11,6 +11,7 @@ const io = new Server(server, { cors: { origin: "*" } });
 app.use(express.json());
 app.use(cors());
 
+app.use('/', (req, res) => res.send(`<h1>Welcome to the Node js Server</h1>`));
 app.use("/api/emails", require("./routes/emailRoutes")(io));
 
 io.on("connection", (socket) => {
